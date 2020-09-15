c83165b43649ca07158bdd907373287e7738460a
m41smith <m41smith@student.bridgew.edu>
2020-09-15 11:06:26 -0400

    introduction to strings

--- Strings.py
+++ Strings.py
@@ -1,7 +1,9 @@
 
 def main():
+    # Now lets find out who you are
+    name = input("What is your name:")
     age = 5
-    str_demo = f"Wow you are {age}, you are growing up"
+    str_demo = f"Wow {name} you are {age}, you are growing up"
     print(str_demo)
 
 main()
