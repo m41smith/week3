3840a0386cc56c648017ea528df7307a024a930d
m41smith <m41smith@student.bridgew.edu>
2020-09-15 11:24:37 -0400

    Simple C to F calculator

---
+++ FtoCConvert.py
@@ -0,0 +1,8 @@
+
+def main():
+    temp_in_c = float(input("What is the Celsius Temp:"))
+    f_temp = temp_in_c * 1.8 + 32
+    print(f" {temp_in_c} degrees celsius is {f_temp} in Fahrenheit")
+
+
+main()
