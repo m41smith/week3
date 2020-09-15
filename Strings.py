bf8f73021a8221e8339539e89c57cfc89912176c
m41smith <m41smith@student.bridgew.edu>
2020-09-15 11:14:37 -0400

    read in numbers

--- Strings.py
+++ Strings.py
@@ -1,9 +1,10 @@
 
 def main():
-    # Now lets find out who you are
-    name = input("What is your name:")
-    age = 5
-    str_demo = f"Wow {name} you are {age}, you are growing up"
+    # Now lets find out who your dog is
+    name = input("What is your dog's name:")
+    age = int(input("How old is your Dog:"))
+    dog_years = age * 7
+    str_demo = f"Wow {name} you are {dog_years} old in dog years"
     print(str_demo)
 
 main()
